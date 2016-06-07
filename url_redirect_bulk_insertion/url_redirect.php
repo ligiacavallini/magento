d7e50ec374d31b0d137869865019b8a97479248c
Ligia Cavallini <ligiapltr@gmail.com>
2016-06-07 17:12:45 -0400

    Add files via upload

---
+++ url_redirect_bulk_insertion/url_redirect.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * 301 Url Redirect Import via CSV file for Magento Enterprise Edition
+ * @author Ligia Cavallini
+ */
+
+//change it to your right path
+require_once 'app/Mage.php';
+
+//set the store id that is going to receive the data
+Mage::app()->setCurrentStore(3);
+
+//path/name of your file
+$file = 'url_redirect.csv';
+
+echo "Running url redirect import for Florida Cooling...\n";
+
+if (($handle = fopen($file, "r")) !== FALSE) {
+
+    while (($data = fgetcsv($handle, 4086, ","))) {
+
+        //check if redirect already exists
+        $load_item = Mage::getSingleton('enterprise_urlrewrite/redirect')
+            ->getCollection()
+            ->addFieldToFilter('identifier', $data[0])
+            ->addFieldToFilter('store_id', '3');
+
+        if(count($load_item)>0){
+            echo "Redirect from " . $data[0] . " to " . $data[1] . " already exists.\n";
+        }else{
+        	//if redirect doesn't exist yet, try to insert
+            try {
+                $redirect = Mage::getSingleton('enterprise_urlrewrite/redirect')
+                    ->setRedirectId(null)
+                    ->setTargetPath($data[1])
+                    ->setIdentifier($data[0])
+                    ->setStoreId(3)
+                    ->setOptions('RP')
+                    ->setDescription('This description is optional');
+
+                $redirect->save();
+
+				//reindex
+                $client = Mage::getModel('enterprise_mview/client');
+                $client->init('enterprise_url_rewrite_redirect');
+                $client->execute('enterprise_urlrewrite/index_action_url_rewrite_redirect_refresh_row',
+                    array('redirect_id' => $redirect->getId())
+                );
+                echo "Imported from " . $data[0] . " to " . $data[1] . " done.\n";
+            } catch (Exception  $e) {
+                echo "Exception Occured: " . $e;
+                echo "Error importing redirect from " . $data[0] . " to " . $data[1] . "\n";
+            }
+        }
+    }
+}else{
+    echo "Error trying to read the file";
+}
